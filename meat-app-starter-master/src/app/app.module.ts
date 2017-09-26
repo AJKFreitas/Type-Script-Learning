67a00ed228897c4885511c0b3fc18b3b128e45ac
Angelo Antonio De Freitas Santos <aasantos6@stefanini.com>
2017-09-26 18:31:36 -0300

    inclusão do projeto exemplo

---
+++ meat-app-starter-master/src/app/app.module.ts
@@ -0,0 +1,28 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { HttpModule } from '@angular/http';
+import { RouterModule } from '@angular/router';
+
+
+import { AppComponent } from './app.component';
+import { HeaderComponent } from 'app/header/header.component';
+import { HomeComponent } from 'app/home/home.component';
+import { AboutComponent } from 'app/about/about.component';
+
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    HeaderComponent,
+    HomeComponent,
+    AboutComponent
+  ],
+  imports: [
+    BrowserModule,
+    HttpModule,
+    RouterModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
