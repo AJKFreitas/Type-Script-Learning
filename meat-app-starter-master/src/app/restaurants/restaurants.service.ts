6f731c496114ea57682866d419ee4b50a13be5f0
AngeloFreitas <angelo-fs@hotmail.com>
2017-10-08 20:25:29 -0300

    Detail restaurant

--- meat-app-starter-master/src/app/restaurants/restaurants.service.ts
+++ meat-app-starter-master/src/app/restaurants/restaurants.service.ts
@@ -13,7 +13,7 @@ export class RestaurantsService {
 
 
     restaurants():Observable< Restaurant[]> {
-      return this.http.get(`${MEAT_API}/restaurants2`)
+      return this.http.get(`${MEAT_API}/restaurants`)
         .map(response => response.json())
         .catch(ErrorHandler.handleError)
     }
