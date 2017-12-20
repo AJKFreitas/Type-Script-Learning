b0ec285c8eee84331572b35980338d5c60ffbc70
Angelo Freitas <ajkfreitas@gmail.com>
2017-12-20 10:19:55 -0200

    componente rating

--- meat-app-starter-master/src/app/shared/rating/rating.component.ts
+++ meat-app-starter-master/src/app/shared/rating/rating.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Output,EventEmitter } from '@angular/core';
 
 @Component({
   selector: 'mt-rating',
@@ -6,12 +6,31 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RatingComponent implements OnInit {
 
+  @Output() rated = new EventEmitter<number>()
   rates: number[]=[1,2,3,4,5];
   rate: number = 0;
+  previosRate: number;
 
   constructor() { }
 
   ngOnInit() {
   }
+  setRate(rt:number){
+    this.rate= rt;
+    this.previosRate = undefined;
+    this.rated.emit(this.rate);
+  }
+  serTemporaryRate(rt){
+    if(this.previosRate === undefined){
+      this.previosRate = this.rate;
+    }
+    this.rate = rt;
+  }
+  clearTemporaryRate(){
+        if(this.previosRate !== undefined){
+          this.rate  = this.previosRate ;
+          this.previosRate = undefined;
+        }
 
+  }
 }
