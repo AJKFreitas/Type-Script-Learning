b04aa9d7a9f833116351085c024a41587e7491c7
Angelo Freitas <ajkfreitas@gmail.com>
2017-12-20 14:14:35 -0200

    Validação de e-mail

--- meat-app-starter-master/src/app/order/order.component.ts
+++ meat-app-starter-master/src/app/order/order.component.ts
@@ -5,6 +5,7 @@ import { OrderService } from 'app/order/order.service';
 import { Order, OrderItem } from 'app/order/order.model';
 import { Router } from '@angular/router';
 import { FormBuilder, FormGroup,Validators } from '@angular/forms';
+import { AbstractControl } from '@angular/forms/src/model';
 
 @Component({
   selector: 'mt-order',
@@ -32,12 +33,25 @@ export class OrderComponent implements OnInit {
     this.orderForm = this.formBuider.group({
       name:this.formBuider.control('',[Validators.required, Validators.minLength(5)]),
       email:this.formBuider.control('',[Validators.required,Validators.pattern(this.emailPattern)]),
-      emailConfirmation:this.formBuider.control('',[Validators.required,Validators.email]),
+      emailConfirmation:this.formBuider.control('',[Validators.required,Validators.pattern(this.emailPattern)]),
       address: this.formBuider.control('',[Validators.required,Validators.minLength(5)]),
       number:this.formBuider.control('',[Validators.required,Validators.pattern(this.numberPattern)]),
       optional:this.formBuider.control(''),
       paymentOption:this.formBuider.control('',[Validators.required])
-    })
+    },{validator:OrderComponent.equalsTo})
+  }
+
+  static equalsTo(group:AbstractControl): {[key:string]:boolean}{
+    
+    const email = group.get('email')
+    const emailConfirmation = group.get('emailConfirmation')
+    if(!email || ! emailConfirmation){
+      return undefined
+    }
+    if(email.value !== emailConfirmation.value){
+      return {emailNotMatch:true}
+    }
+    return undefined
   }
 
   itemsValue():number{
