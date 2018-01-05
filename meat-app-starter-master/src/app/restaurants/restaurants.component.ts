a845e0faa136450236a9cd0ce4ea14b487a1b7af
Angelo Freitas <ajkfreitas@gmail.com>
2018-01-05 10:41:20 -0200

    Animate restaurant

--- meat-app-starter-master/src/app/restaurants/restaurants.component.ts
+++ meat-app-starter-master/src/app/restaurants/restaurants.component.ts
@@ -1,14 +1,17 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 import { Restaurant } from './/restaurant/restaurant.model';
 import { RestaurantsService } from 'app/restaurants/restaurants.service';
+
+
 
 @Component({
   selector: 'mt-restaurants',
   templateUrl: './restaurants.component.html',
+ 
 })
 export class RestaurantsComponent implements OnInit {
 
-  restaurants: Restaurant[]
+ @Input() restaurants: Restaurant[]
 
   constructor(private restaurantsService: RestaurantsService) { }
 
