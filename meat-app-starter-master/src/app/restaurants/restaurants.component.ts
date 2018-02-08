a1f5a3566e0042eb58dcdfd339b0ae9a611c978a
Angelo Freitas <ajkfreitas@gmail.com>
2018-02-08 12:18:47 -0200

    implementação da barra de busca dinamica

--- meat-app-starter-master/src/app/restaurants/restaurants.component.ts
+++ meat-app-starter-master/src/app/restaurants/restaurants.component.ts
@@ -1,21 +1,61 @@
+import { Observable } from 'rxjs/Observable';
+import { FormBuilder, FormControl, FormGroup } from '@angular/forms';
 import { Component, OnInit, Input } from '@angular/core';
 import { Restaurant } from './/restaurant/restaurant.model';
 import { RestaurantsService } from 'app/restaurants/restaurants.service';
+import {trigger, state, style, transition, animate} from "@angular/animations";
 
-
+import 'rxjs/add/operator/switchMap';
+import 'rxjs/add/operator/do';
+import 'rxjs/add/operator/catch';
+import 'rxjs/add/Observable/from';
+import 'rxjs/add/operator/debounceTime';
+import 'rxjs/add/operator/distinctUntilChanged'
 
 @Component({
   selector: 'mt-restaurants',
   templateUrl: './restaurants.component.html',
- 
+ animations:[
+   trigger('toggleSearch', [
+     state('hidden', style({
+       opacity: 0,
+       "max-hight": "0px",
+       "margin-top": "0px"
+     })),
+     state('visible', style({
+       opacity: 1,
+       "max-height": "70px",
+       "margin-top": "20px"
+     })),
+     transition('* => *', animate('250ms 0s ease-in-out'))
+   ])
+ ]
 })
 export class RestaurantsComponent implements OnInit {
 
+  searchBarState = 'hidden'
  @Input() restaurants: Restaurant[]
 
-  constructor(private restaurantsService: RestaurantsService) { }
+  searchForm: FormGroup
+  searchControl:FormControl
+
+  constructor(private restaurantsService: RestaurantsService,
+                      private fb:FormBuilder) { }
 
   ngOnInit() {
+    
+      this.searchControl = this.fb.control('')
+      this.searchForm = this.fb.group({
+        searchControl : this.searchControl
+      })
+
+      this.searchControl.valueChanges
+        .debounceTime(500)
+        .distinctUntilChanged()
+        .switchMap(searchTerm => // controla o tempo de resquisição da web 
+          this.restaurantsService.restaurants(searchTerm)
+        .catch(error => Observable.from([])))
+        .subscribe(restaurants => this.restaurants = restaurants)
       this.getRestaurants();
   }
 
@@ -25,5 +65,9 @@ export class RestaurantsComponent implements OnInit {
           .subscribe(
             restaurants => this.restaurants = restaurants
           );
+    }
+
+    toggleSearch(){
+      this.searchBarState = this.searchBarState === 'hidden' ? 'visible' : 'hidden';
     }
 }
