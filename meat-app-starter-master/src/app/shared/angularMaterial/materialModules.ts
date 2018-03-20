56565c6ac6c8262ae962a90b40713752ae7153f2
Angelo Freitas <ajkfreitas@gmail.com>
2018-03-20 14:55:51 -0300

    teste de cadastro

---
+++ meat-app-starter-master/src/app/shared/angularMaterial/materialModules.ts
@@ -0,0 +1,8 @@
+import { NgModule } from '@angular/core';
+
+@NgModule({
+  declarations: [],
+  // imports: [MatButtonModule, MatCheckboxModule],
+  // exports: [MatButtonModule, MatCheckboxModule],
+})
+export class MaterialModules { }
